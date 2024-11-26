32d8293cb44dcdf06b1be738ec5beffe3ad17fda
Allan <allan.mendes0976@gmail.com>
2024-11-26 13:04:40 -0300

    finalizando

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+import * as cartService from "./services/cart.js";
 import createItem from "./services/item.js";
 
-const cart = [];
+const myCart = [];
+const myWhishList = [];
 
 console.log("bem vindo ao seu carrinho de compras da Shopee!");
 
-const item1 = await createItem("ventilador", 180.0, 1);
+const item1 = await createItem("ventilador", 180.0, 3);
 const item2 = await createItem("cama box", 500, 2);
 
-console.log(item2.subtotal());
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myCart, item2);
+
+//await cartService.removeItem(myCart, 1);
+await cartService.removeItem(myCart, item1);
+await cartService.removeItem(myCart, item1);
+await cartService.removeItem(myCart, item1);
+
+await cartService.displayCart(myCart);
+
+// await cartService.deleteItem(myCart, item1.name);
+// await cartService.deleteItem(myWhishList, item2);
+
+cartService.calculateTotal(myCart);
--- src/services/cart.js
+++ src/services/cart.js
@@ -1,13 +1,61 @@
 // adicionar um item
 
-async function addItem(userCart) {}
+async function addItem(userCart, item) {
+  userCart.push(item);
+}
 
 // Deletar item do carrinho
-async function deleteItem(userCart, name) {}
+async function deleteItem(userCart, name) {
+  const index = userCart.findIndex((item) => item.name === name);
+
+  if (index !== -1) {
+    userCart.splice(index, 1);
+  }
+}
 
 //Remover um item - diminui um item
 
-async function removeItem(userCart, index) {}
+// async function removeItem(userCart, index) {
+//   if (index >= 0 && index < userCart.length) {
+//     const deleteIndex = index - 1;
+//     userCart.splice(deleteIndex, 1);
+//   }
+// }
+
+async function removeItem(userCart, item) {
+  const indexFound = userCart.findIndex((p) => p.name === item.name);
+  if (indexFound === -1) {
+    console.log("Item não encontrado.");
+    return;
+  }
+
+  if (userCart[indexFound].quantity > 1) {
+    userCart[indexFound].quantity -= 1;
+    return;
+  }
+
+  if (userCart[indexFound].quantity == 1) {
+    userCart.splice(indexFound, 1);
+    return;
+  }
+}
 
 // Calcular o total do carrinho
-async function calculateTotal(userCart) {}
+async function calculateTotal(userCart) {
+  console.log("\nValor total:");
+  const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
+  console.log(`Total: ${result} 🎁`);
+}
+
+async function displayCart(userCart) {
+  console.log("\nCarrinho de Compras: ");
+  userCart.forEach((item, index) => {
+    console.log(
+      `${index + 1}. ${item.name} - R$ ${item.price} | ${
+        item.quantity
+      }x | Subtotal = ${item.subtotal()}`
+    );
+  });
+}
+
+export { addItem, calculateTotal, deleteItem, removeItem, displayCart };
